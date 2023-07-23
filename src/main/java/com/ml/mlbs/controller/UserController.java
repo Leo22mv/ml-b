6a0577834e08dbb6d88f233335c004c19738f105
Kbza <g.leonel.m17@gmail.com>
2023-07-23 06:19:45 -0300

    0.42

--- src/main/java/com/ml/mlbs/controller/UserController.java
+++ src/main/java/com/ml/mlbs/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -69,6 +70,11 @@ public class UserController {
 //         }
 //     }
 // }
+
+    @GetMapping ("/usuarios")
+    public List<User> getUsers() {
+        return iuserService.getUsers();
+    }
     
 }
 
