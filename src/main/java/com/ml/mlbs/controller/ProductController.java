6bc33e321204e47e488f08cabd4b30ea5f270181
Kbza <g.leonel.m17@gmail.com>
2023-07-25 06:24:06 -0300

    0.55

--- src/main/java/com/ml/mlbs/controller/ProductController.java
+++ src/main/java/com/ml/mlbs/controller/ProductController.java
@@ -3,9 +3,12 @@ package com.ml.mlbs.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,8 +33,14 @@ public class ProductController {
         iproductService.saveProduct(product);
     }
 
-    @DeleteMapping ("/productos/eliminar")
-    public void deleteProduct(@RequestBody Long id) {
-        iproductService.deleteProduct(id);
+    @DeleteMapping("/productos/{id}")
+    public ResponseEntity<Void> eliminarProducto(@PathVariable Long id) {
+        try {
+            iproductService.deleteProduct(id);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
