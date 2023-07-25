bb57b5c2383caafc327c2146413f30a9674481cf
Kbza <g.leonel.m17@gmail.com>
2023-07-25 06:16:27 -0300

    0.55

--- src/main/java/com/ml/mlbs/controller/ProductController.java
+++ src/main/java/com/ml/mlbs/controller/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,7 +30,7 @@ public class ProductController {
         iproductService.saveProduct(product);
     }
 
-    @PostMapping ("/productos/eliminar")
+    @DeleteMapping ("/productos/eliminar")
     public void deleteProduct(@RequestBody Long id) {
         iproductService.deleteProduct(id);
     }
