5df8da985048c0c103088dcca8925e8d8027c588
Kbza <g.leonel.m17@gmail.com>
2023-07-26 16:45:00 -0300

    0.75

--- src/main/java/com/ml/mlbs/MlBSApplication.java
+++ src/main/java/com/ml/mlbs/MlBSApplication.java
@@ -40,27 +40,27 @@ public class MlBSApplication {
 							.build()))
 					.build();
 
-					UserEntity userEntity2 = UserEntity.builder()
-					.email("anyi@mail.com")
-					.username("anyi")
-					.password(passwordEncoder.encode("1234"))
-					.roles(Set.of(RoleEntity.builder()
-							.name(ERole.valueOf(ERole.USER.name()))
-							.build()))
-					.build();
+					// UserEntity userEntity2 = UserEntity.builder()
+					// .email("anyi@mail.com")
+					// .username("anyi")
+					// .password(passwordEncoder.encode("1234"))
+					// .roles(Set.of(RoleEntity.builder()
+					// 		.name(ERole.valueOf(ERole.USER.name()))
+					// 		.build()))
+					// .build();
 
-					UserEntity userEntity3 = UserEntity.builder()
-							.email("andrea@mail.com")
-							.username("andrea")
-							.password(passwordEncoder.encode("1234"))
-							.roles(Set.of(RoleEntity.builder()
-									.name(ERole.valueOf(ERole.INVITED.name()))
-									.build()))
-							.build();
+					// UserEntity userEntity3 = UserEntity.builder()
+					// 		.email("andrea@mail.com")
+					// 		.username("andrea")
+					// 		.password(passwordEncoder.encode("1234"))
+					// 		.roles(Set.of(RoleEntity.builder()
+					// 				.name(ERole.valueOf(ERole.INVITED.name()))
+					// 				.build()))
+					// 		.build();
 				
 					userRepository.save(userEntity);
-					userRepository.save(userEntity2);
-					userRepository.save(userEntity3);
+					// userRepository.save(userEntity2);
+					// userRepository.save(userEntity3);
 		};
 	}
 	
