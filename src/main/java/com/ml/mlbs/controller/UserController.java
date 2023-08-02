ebd66fb88282fdf7e9057ea17cbdb027cf66b903
Kbza <g.leonel.m17@gmail.com>
2023-08-02 07:33:30 -0300

    0.95

--- src/main/java/com/ml/mlbs/controller/UserController.java
+++ src/main/java/com/ml/mlbs/controller/UserController.java
@@ -104,6 +104,7 @@ public class UserController {
 // }
 
     @GetMapping ("/usuarios")
+    @PreAuthorize("hasRole('ADMIN')")
     public List<UserEntity> getUsers() {
         return iuserService.getUsers();
     }
