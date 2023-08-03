19e4bc6be1f1c4e83f05e6e422178c90050d827f
Kbza <g.leonel.m17@gmail.com>
2023-08-03 08:56:53 -0300

    0.95

--- src/main/java/com/ml/mlbs/security/CorsConfig.java
+++ src/main/java/com/ml/mlbs/security/CorsConfig.java
@@ -1,19 +1,19 @@
-// package com.ml.mlbs.security;
+package com.ml.mlbs.security;
 
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// @Configuration
-// public class CorsConfig implements WebMvcConfigurer {
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
 
-//     @Override
-//     public void addCorsMappings(CorsRegistry registry) {
-//         registry.addMapping("/**")
-//                 // .allowedOrigins("http://localhost:4200")
-//                 .allowedOrigins("https://e-commerce-f.web.app")
-//                 .allowedMethods("*")
-//                 .allowedHeaders("*");
-//     }
-// }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                // .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("https://e-commerce-f.web.app")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
 
