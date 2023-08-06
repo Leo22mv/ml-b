09b6becafa21c1d8ea04c726785b789b0ba6ed11
Kbza <g.leonel.m17@gmail.com>
2023-08-06 15:08:31 -0300

    0.95

--- src/main/java/com/ml/mlbs/security/CorsConfig.java
+++ src/main/java/com/ml/mlbs/security/CorsConfig.java
@@ -1,18 +1,18 @@
-package com.ml.mlbs.security;
+// package com.ml.mlbs.security;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
+// @Configuration
+// public class CorsConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://e-commerce-f.web.app")
-                .allowedMethods("*")
-                .allowedHeaders("*");
-    }
-}
+//     @Override
+//     public void addCorsMappings(CorsRegistry registry) {
+//         registry.addMapping("/**")
+//                 .allowedOrigins("https://e-commerce-f.web.app")
+//                 .allowedMethods("*")
+//                 .allowedHeaders("*");
+//     }
+// }
 
--- src/main/java/com/ml/mlbs/security/SecurityConfig.java
+++ src/main/java/com/ml/mlbs/security/SecurityConfig.java
@@ -51,11 +51,11 @@ public class SecurityConfig {
                 .cors()
                 .and()
                 .csrf().disable()
-                .authorizeHttpRequests(auth -> {
+                // .authorizeHttpRequests(auth -> {
                 //     auth.requestMatchers("/hola").permitAll();
                 //     auth.anyRequest().authenticated();
-                    auth.anyRequest().permitAll();
-                })
+                    // auth.anyRequest().permitAll();
+                // })
                 .sessionManagement(session -> {
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
                 })
@@ -112,7 +112,6 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        // configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200")); // Agrega aquí los orígenes permitidos
         configuration.setAllowedOrigins(Arrays.asList("https://e-commerce-f.web.app")); // Agrega aquí los orígenes permitidos
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Agrega los métodos permitidos
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type")); // Agrega los encabezados permitidos
